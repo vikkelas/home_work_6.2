2a24b7272f69cb7e83bd2f6bc4f1672ad689b250
Vikkelas <bass89@mail.ru>
2021-11-06 13:01:33 +0300

    eslint

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,43 +1,43 @@
-import returnArrAttacks from '../app.js'
+import returnArrAttacks from '../app.js';
 
 const character = {
-	name: 'Лучник',
-	type: 'Bowman',
-	health: 50,
-	level: 3,
-	attack: 40,
-	defence: 10,
-	special: [
-		{
-			id: 8,
-			name: 'Двойной выстрел',
-			icon: 'http://...',
-			description: 'Двойной выстрел наносит двойной урон',
-		},
-		{
-			id: 9,
-			name: 'Нокаутирующий удар',
-			icon: 'http://...',
-		},
-	],
-}
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+    },
+  ],
+};
 
 const correctArr = [
-	{
-		id: 8,
-		name: 'Двойной выстрел',
-		description: 'Двойной выстрел наносит двойной урон',
-		icon: 'http://...',
-	},
-	{
-		id: 9,
-		name: 'Нокаутирующий удар',
-		description: 'Описание недоступно',
-		icon: 'http://...',
-	},
-]
+  {
+    id: 8,
+    name: 'Двойной выстрел',
+    description: 'Двойной выстрел наносит двойной урон',
+    icon: 'http://...',
+  },
+  {
+    id: 9,
+    name: 'Нокаутирующий удар',
+    description: 'Описание недоступно',
+    icon: 'http://...',
+  },
+];
 
 test('check function to return correct arr', () => {
-	const result = returnArrAttacks(character)
-	expect(result).toEqual(correctArr)
-})
+  const result = returnArrAttacks(character);
+  expect(result).toEqual(correctArr);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,15 @@
 export default function returnArrAttacks(object) {
-	const arrSpecialAttacks = []
-	object.special.forEach(element => {
-		const { id, name, description = 'Описание недоступно', icon } = element
-		const specialAttack = {}
-		specialAttack.id = id
-		specialAttack.name = name
-		specialAttack.description = description
-		specialAttack.icon = icon
-		arrSpecialAttacks.push(specialAttack)
-	})
-	return arrSpecialAttacks
+  const arrSpecialAttacks = [];
+  object.special.forEach((element) => {
+    const {
+      id, name, description = 'Описание недоступно', icon,
+    } = element;
+    const specialAttack = {};
+    specialAttack.id = id;
+    specialAttack.name = name;
+    specialAttack.description = description;
+    specialAttack.icon = icon;
+    arrSpecialAttacks.push(specialAttack);
+  });
+  return arrSpecialAttacks;
 }
